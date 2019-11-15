54b8e2a51c8f81c8d1a97f0ffea286b332f13680
mutisantos <mutisantos@hotmail.com>
2019-11-15 14:18:03 -0500

    Bean mapping with restController

--- src/main/java/com/kanionland/rest/webservices/restfulwebservices/beans/HelloWorldBean.java
+++ src/main/java/com/kanionland/rest/webservices/restfulwebservices/beans/HelloWorldBean.java
@@ -1,5 +1,21 @@
 package com.kanionland.rest.webservices.restfulwebservices.beans;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+// This bean is being returned as a whole object in JSON Format. It's going to show only those methods with a Getter
+// implemented
 public class HelloWorldBean {
 
+   @Getter
+   private final String message;
+   // @Getter
+   private final String issue;
+
+   public HelloWorldBean(final String message) {
+      this.message = message;
+      this.issue = "0";
+   }
 }
--- src/main/java/com/kanionland/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/kanionland/rest/webservices/restfulwebservices/controllers/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.kanionland.rest.webservices.restfulwebservices.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kanionland.rest.webservices.restfulwebservices.beans.HelloWorldBean;
+
 //This annotation tells SpringMVC that this controller will handle REST requests
 @RestController
 public class HelloWorldController {
@@ -11,6 +13,11 @@ public class HelloWorldController {
    @GetMapping(path = "/hello-world") // => annotations with the method within
    public String helloWorld() {
       return "Hello World";
+   }
+
+   @GetMapping(path = "/hello-world-bean") // => annotations with the method within
+   public HelloWorldBean helloWorldBean() {
+      return new HelloWorldBean("Hello World");
    }
 
 }
